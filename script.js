777e67ec36ed8ef79b5063a94e42eaf205c13c65
gmastellone <gmastellone94@gmail.com>
2021-09-14 10:36:14 +0200

    1st Commit

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
+
 function compute()
 {
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
     p = document.getElementById("principal").value;
-    
-}
+
+    //Check if Amount field is empty
+    if(principal <= 0){
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        console.log("negativo")
+        }
+    //If all is well, start rate calculation.
+    document.getElementById("result").innerHTML="If you deposit <mark>"+principal+"</mark>,\<br\>at an interest rate of <mark>"+rate+"%</mark>\<br\>You will receive an amount of <mark>"+principal+interest+"</mark> ,\<br\>in the year <mark>"+year+"</mark>\<br\>";
+    console.log("ok");
+};
+
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+};
+
+
         
