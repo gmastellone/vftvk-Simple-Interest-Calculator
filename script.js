a441f561694c4674c92ebb16915a2f82d83ba4bc
gmastellone <gmastellone94@gmail.com>
2021-09-14 10:43:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -13,9 +13,10 @@ function compute()
         alert("Enter a positive number");
         document.getElementById("principal").focus();
         return;
-        }
+        } else {
     //If all is well, start rate calculation.
     document.getElementById("result").innerHTML="If you deposit <mark>"+principal+"</mark>,\<br\>at an interest rate of <mark>"+rate+"%</mark>\<br\>You will receive an amount of <mark>"+principal+interest+"</mark> ,\<br\>in the year <mark>"+year+"</mark>\<br\>";
+}
 };
 
 function updateRate() 
